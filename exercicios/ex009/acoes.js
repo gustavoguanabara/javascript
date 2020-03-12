f1bfa7f0edecff49e380268602380d0028098de6
Gustavo Guanabara <professorguanabara@gmail.com>
2020-03-12 20:06:34 -0300

    update exercises

---
+++ exercicios/ex009/acoes.js
@@ -0,0 +1,15 @@
+function acao1() {
+    window.alert('Clicou no primeiro botão')
+}
+
+function acao2() {
+    window.alert('Clicou no segundo botão')
+}
+
+function acao3() {
+    window.alert('Clicou no terceiro botão')
+}
+
+function acao4() {
+    window.alert('Clicou no quarto botão')
+}
