03e4aaaa8fdb6c662e2f2281b6e785bcb2a76bec
Gustavo Guanabara <professorguanabara@gmail.com>
2020-03-14 14:47:51 -0300

    exercícios atualizados

---
+++ exercicios/ex020/script.js
@@ -0,0 +1,25 @@
+function estação() { // Sim, é possível usar acentuação
+    let mês = prompt('Digite o mês em extenso (ex: Setembro)')
+    let saída = document.querySelector('section#saida')
+    let estação
+    switch (mês.toUpperCase()) { // O método toUpperCase() transforma todas as letras de uma string para maiúsculas
+        case 'JANEIRO': case 'FEVEREIRO': case 'MARÇO': // Podemos testar múltiplos casos em uma mesma linha, desse jeito
+            estação = 'INVERNO'
+            break // Nunca se esqueça do break!!!
+        case 'ABRIL': case 'MAIO': case 'JUNHO':
+            estação = 'PRIMAVERA'
+            break
+        case 'JULHO': case 'AGOSTO': case 'SETEMBRO':
+            estação = 'VERÃO'
+            break
+        case 'OUTUBRO': case 'NOVEMBRO': case 'DEZEMBRO':
+            estação = 'OUTONO'
+            break
+        default:
+            estação = 'INDEFINIDA'
+            break
+    }
+    saída.innerHTML = `<p>No mês de <mark>${mês}</mark>, estamos na estação <mark><strong>${estação}</strong></mark>.</p>`
+}
+
+// Sugestão de melhoria: refaça esse programa para que ele aceite tanto o mês por extenso quanto o número do mês.
