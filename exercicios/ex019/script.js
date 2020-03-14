5e00f423d9b81aebc050ed9986a4bc268d84293f
Gustavo Guanabara <professorguanabara@gmail.com>
2020-03-14 14:19:35 -0300

    exercícios atualizados

--- exercicios/ex019/script.js
+++ exercicios/ex019/script.js
@@ -5,10 +5,10 @@ function calc() {
 
     let saida = document.getElementById('saida')
     saida.innerHTML = `<h2>Calculando...</h2>`
-    switch (op) {
+    switch (op) { // A estrutura switch basicamente permite você testar valores dentro de uma variável ou expressão. Ela é compatível apenas com números inteiros e strings
         case 1:
             saida.innerHTML += `<p>${n1} + ${n2} = <strong>${n1+n2}</strong></p>`
-            break
+            break // O break é obrigatório em cada case. Se não for colocado, acontecerá a execução de vários comandos indesejados de outros cases
         case 2:
             saida.innerHTML += `<p>${n1} - ${n2} = <strong>${n1-n2}</strong></p>`
             break
@@ -16,7 +16,10 @@ function calc() {
             saida.innerHTML += `<p>${n1} x ${n2} = <strong>${n1*n2}</strong></p>`
             break
         case 4:
-            saida.innerHTML += `<p>${n1} / ${n2} = <strong>${n1/n2}</strong></p>`
+            saida.innerHTML += `<p>${n1} / ${n2} = <strong>${(n1/n2).toLocaleString('pt-BR')}</strong></p>` // O método toLocaleString() é muito útil para mostrar números de forma mais compatível com o que usamos aqui no Brasil.
+            break
+        default: // Se o usuário não digitar nenhum dos valores acima, vai cair aqui
+            saida.innerHTML += `<p>OPÇÃO INVÁLIDA! Você digitou ${n1} e ${n2}, mas não sei o que fazer com eles. </p>`
             break
     }
 }
