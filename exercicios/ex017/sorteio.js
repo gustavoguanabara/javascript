e21d265b0d6d79b7ec6e6c4ea4afb01aceb8e9fd
Gustavo Guanabara <professorguanabara@gmail.com>
2020-03-15 17:33:18 -0300

    exercises updates

--- exercicios/ex017/sorteio.js
+++ exercicios/ex017/sorteio.js
@@ -1,3 +1,19 @@
+/*
+    ## IMPORTANTE ##
+    Você tem todo o direito de usar esse material 
+    para seu próprio aprendizado. Professores também 
+    podem ter acesso a todo o conteúdo e usá-los com 
+    seus alunos. Porém todos os que usarem esse 
+    material - seja para qual for a finalidade - deverão 
+    manter a referência ao material original, disponível 
+    em https://github.com/gustavoguanabara/javascript. Este 
+    material não poderá ser utilizado em nenhuma hipótese 
+    para ser replicada - integral ou parcialmente - 
+    por autores/editoras/instituições para criar livros 
+    ou apostilas, com finalidades de obter ganho financeiro 
+    com ele.
+*/
+
 function gerar() {
     let min = 1
     let max = 100
--- exercicios/ex018/adivinha.js
+++ exercicios/ex018/adivinha.js
@@ -1,3 +1,19 @@
+/*
+    ## IMPORTANTE ##
+    Você tem todo o direito de usar esse material 
+    para seu próprio aprendizado. Professores também 
+    podem ter acesso a todo o conteúdo e usá-los com 
+    seus alunos. Porém todos os que usarem esse 
+    material - seja para qual for a finalidade - deverão 
+    manter a referência ao material original, disponível 
+    em https://github.com/gustavoguanabara/javascript. Este 
+    material não poderá ser utilizado em nenhuma hipótese 
+    para ser replicada - integral ou parcialmente - 
+    por autores/editoras/instituições para criar livros 
+    ou apostilas, com finalidades de obter ganho financeiro 
+    com ele.
+*/
+
 // Todas as variáveis declaradas aqui em cima - fora das funções - são consideradas GLOBAIS e funcionam dentro de qualquer outra função!
 let res = document.querySelector('section#result')
 let computador = 0
