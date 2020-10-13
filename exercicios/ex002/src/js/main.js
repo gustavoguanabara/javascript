231a5c4a9bee532a10f7c8a306450d80fd2c1cdb
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-10-13 17:17:34 -0300

    Separei os códigos em arquivos diferentes em um padrão de design

---
+++ exercicios/ex002/src/js/main.js
@@ -0,0 +1,11 @@
+(function(doc) {
+
+    const button = doc.querySelector('#button');
+
+    button.addEventListener('click', () => {
+
+        alert('Você clicou no botão!');
+
+    });
+
+})(document);
